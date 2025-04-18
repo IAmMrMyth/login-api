5bef15e3164a7e334bcda191665df8fe2c3f5253
iammrmyth <iammrmyth@gmail.com>
2025-04-18 05:16:56 +0330

    implement custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=10, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'phone_number'
+
+    def __str__(self):
+        return self.username
