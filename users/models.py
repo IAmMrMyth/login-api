865946b98bc330cc0a3e0ddd6d76902e25826ebc
iammrmyth <iammrmyth@gmail.com>
2025-04-18 05:30:45 +0330

    refactor CustomUser model for consistent formatting and improved readability

--- users/models.py
+++ users/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser 
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
-    email = models.EmailField("ایمیل",unique=True)
-    phone_number = models.CharField("شماره تلفن",max_length=10, unique=True)
-    first_name = models.CharField("نام",max_length=100)
-    last_name = models.CharField("نام خانوادگی",max_length=100)
-    is_verified = models.BooleanField("تایید شده",default=False)
-    
-    USERNAME_FIELD = 'phone_number'
+    email = models.EmailField("ایمیل", unique=True)
+    phone_number = models.CharField("شماره تلفن", max_length=10, unique=True)
+    first_name = models.CharField("نام", max_length=100)
+    last_name = models.CharField("نام خانوادگی", max_length=100)
+    is_verified = models.BooleanField("تایید شده", default=False)
+
+    USERNAME_FIELD = "phone_number"
 
     def __str__(self):
         return self.username
