ea827694400e4e784afed1b15e29fee72274573a
iammrmyth <iammrmyth@gmail.com>
2025-04-18 05:21:39 +0330

    update CustomUser model to include localized field labels

--- users/models.py
+++ users/models.py
@@ -4,13 +4,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=10, unique=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_verified = models.BooleanField(default=False)
+    email = models.EmailField("ایمیل",unique=True)
+    phone_number = models.CharField("شماره تلفن",max_length=10, unique=True)
+    first_name = models.CharField("نام",max_length=100)
+    last_name = models.CharField("نام خانوادگی",max_length=100)
+    is_verified = models.BooleanField("تایید شده",default=False)
     
     USERNAME_FIELD = 'phone_number'
 
