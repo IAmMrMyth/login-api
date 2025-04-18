3f9a53a36b2877cbc20de3fd121e79f699f47ddc
iammrmyth <iammrmyth@gmail.com>
2025-04-18 08:43:29 +0330

    implement CustomUserManager for user creation and management in CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("شماره تلفن الزامی است")
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(phone_number, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -10,6 +25,8 @@ class CustomUser(AbstractUser):
     first_name = models.CharField("نام", max_length=100)
     last_name = models.CharField("نام خانوادگی", max_length=100)
     is_verified = models.BooleanField("تایید شده", default=False)
+    username = None
+    objects = CustomUserManager()
 
     USERNAME_FIELD = "phone_number"
 
